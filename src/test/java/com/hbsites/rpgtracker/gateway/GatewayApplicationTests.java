9cb39a7f1ec20e5e3a0a9682f7ca4d882478b070
Ravi Batistel <ravi.batistel@outlook.com>
2023-07-06 11:32:14 -0300

    Initial commit

---
+++ src/test/java/com/hbsites/rpgtracker/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbsites.rpgtracker.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
